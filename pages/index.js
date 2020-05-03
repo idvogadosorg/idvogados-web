42095ffd337eaa3b9738a1f6380f3dbdd70465e9
Gabriel Vilches <vilchesalves@gmail.com>
2020-05-03 21:04:36 +1200

    add @material-ui/core

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       </Head>
 
       <main>
